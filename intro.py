692b80847611529550b7f6e23e421cb54665e0f0
DAFFADAULAY <daffadaulay1999@gmail.com>
2024-09-04 19:29:37 +0700

    first commit

---
+++ intro.py
@@ -0,0 +1,18 @@
+#Deklarasi Variabel
+a = float(10.5)
+b = 5
+c = a*b
+# Operation (operasi) -> tambah (+), kurang (-), bagi (/), kali (*), modulo (%), pangkat (**)
+#print(c)
+
+perkalian = a * b
+pembagian = a / b
+pengurangan = a - b
+modulo = a % b
+pangkat = a ** b
+
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
